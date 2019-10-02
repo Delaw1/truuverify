6613811c7f34c6d889a1bd41452ea390d469f577
ebukun <ebukun.folami@gmail.com>
2019-10-02 08:06:29 -0700

    Created component folder and created the layout folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends React.Component{
-  constructor(){
-    super()
+class App extends React.Component {
+  constructor() {
+    super();
   }
-  
- 
-  render(){
-      return(
-        <div className="App">
+
+  render() {
+    return (
+      <div className="App">
         <p>This is my first props</p>
       </div>
-      )
+    );
   }
 }
 
