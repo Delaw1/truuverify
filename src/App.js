6427bda2e62dbb459ab8f985f1ec10e8ce3abe90
Delaw1 <lawrenceajayi481@gmail.com>
2019-10-02 14:26:06 +0100

    created

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+  constructor(){
+    super()
+  }
+  
+ 
+  render(){
+      return(
+        <div className="App">
+        <p>This is my first props</p>
+      </div>
+      )
+  }
+}
+
+export default App;
